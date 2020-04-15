59e0c209912e55a67f5d1f26605fdd9dc52eca2f
Emre Karaca <emre.karaca@easybell.de>
2020-04-15 18:32:48 +0200

    Added Note Length

--- src/index.js
+++ src/index.js
@@ -5,31 +5,43 @@ import $ from "jquery";
 // * Vanilla JS
 var myNoteElement = document.getElementById("my-note");
 var note = "";
+var noteLength = note.length;
+document.getElementById("viewer--length").innerHTML = noteLength;
 
 var storedNote = localStorage.getItem("note");
 if (storedNote) {
   note = storedNote;
   myNoteElement.value = note;
   document.getElementById("viewer--text").innerHTML = note;
+  noteLength = note.length;
+  document.getElementById("viewer--length").innerHTML = noteLength;
 }
 
 myNoteElement.addEventListener("keyup", function(e) {
   note = e.target.value;
   document.getElementById("viewer--text").innerHTML = note;
   localStorage.setItem("note", note);
+  noteLength = note.length;
+  document.getElementById("viewer--length").innerHTML = noteLength;
 });
 
 // * jQuery
 var note2 = "";
+var noteLength2 = note2.length;
+document.getElementById("viewer--length").innerHTML = noteLength2;
 
 var storedNote2 = localStorage.getItem("note");
 if (storedNote2) {
   note2 = storedNote2;
   $("#my-note").val(note2);
   $("#viewer--text").html(note2);
+  noteLength2 = note2.length;
+  document.getElementById("viewer--length").innerHTML = noteLength2;
 }
 
 $("#my-note").keyup(function(e) {
   note2 = e.target.value;
   $("#viewer--text").html(note2);
+  noteLength2 = note2.length;
+  document.getElementById("viewer--length").innerHTML = noteLength2;
 });
