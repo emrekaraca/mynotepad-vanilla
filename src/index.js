3cdd37755a43aa7cb0948bf8411dddda32bc7194
Emre Karaca <creagas@googlemail.com>
2020-04-15 17:26:52 +0200

    Saving to localStorage

--- src/index.js
+++ src/index.js
@@ -3,19 +3,33 @@ import "./styles.css";
 import $ from "jquery";
 
 // * Vanilla JS
-if (true) {
-  var myNoteElement = document.getElementById("my-note");
+var myNoteElement = document.getElementById("my-note");
+var note = "";
 
-  myNoteElement.addEventListener("keyup", function(e) {
-    var note = e.target.value;
-    document.getElementById("viewer--text").innerHTML = note;
-  });
+var storedNote = localStorage.getItem("note");
+if (storedNote) {
+  note = storedNote;
+  myNoteElement.value = note;
+  document.getElementById("viewer--text").innerHTML = note;
 }
+
+myNoteElement.addEventListener("keyup", function(e) {
+  note = e.target.value;
+  document.getElementById("viewer--text").innerHTML = note;
+  localStorage.setItem("note", note);
+});
 
 // * jQuery
-if (false) {
-  $("#my-note").keyup(function(e) {
-    var note = e.target.value;
-    $("#viewer--text").html(note);
-  });
+var note2 = "";
+
+var storedNote2 = localStorage.getItem("note");
+if (storedNote2) {
+  note2 = storedNote2;
+  $("#my-note").val(note2);
+  $("#viewer--text").html(note2);
 }
+
+$("#my-note").keyup(function(e) {
+  note2 = e.target.value;
+  $("#viewer--text").html(note2);
+});
